f17a690c1865a307f9803d04ce84c713f76d3cfe
Paulo Franca <admpaulofranca@gmail.com>
2022-04-06 20:26:42 -0300

    FOR decrescente

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,7 +7,7 @@ public class PrimeiraClasseJava {
 
 		/*Estrutura de repetição FOR*/
 		
-		for (int numero = 0; numero <=10; numero ++) {
+		for (int numero = 10; numero >=0; numero --) {
 			System.out.println("Número atual é : " + numero);
 		}
 	}
