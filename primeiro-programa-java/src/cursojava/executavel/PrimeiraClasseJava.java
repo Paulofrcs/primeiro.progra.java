6d57edc97d56b716e80e04c49b83995151e16dbc
Paulo Franca <admpaulofranca@gmail.com>
2022-04-07 01:25:25 -0300

    cursojava.classes.Aluno

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,42 +1,22 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
-
-import org.graalvm.compiler.lir.StandardOp.JumpOp;
+import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
 
 	/* Main é um método auto executável de Java */
 	public static void main(String[] args) {
 
+	
+		Aluno aluno1 = new Aluno();/*Aqui será o João*/
 		
-		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
-		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
-		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
-		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
+		Aluno aluno2 = new Aluno(); /*Aqui será o Pedro*/
 		
+		Aluno aluno3 = new Aluno(); /*Aqui será o Alex*/
 		
+		Aluno aluno4 = new Aluno("Mara");
 		
-		double nNota1 = Double.parseDouble(nota1);
-		double nNota2 = Double.parseDouble(nota2);
-		double nNota3 = Double.parseDouble(nota3);
-		double nNota4 = Double.parseDouble(nota4);
-		
-		double media = (nNota1 + nNota2 + nNota3 + nNota4) / 4;
-		
-		/*Média para aprovação é 70*/
-		
-		if (media >= 50) {
-			
-			if (media >= 70) {
-				JOptionPane.showConfirmDialog(null, "O aluno está aprovado com média de :" + media);
-			}else {
-				JOptionPane.showConfirmDialog(null, "O aluno está em recueração com média de :" + media);
-			}
-		}else {
-			JOptionPane.showConfirmDialog(null, "Aluno está reprovado com média de : " + media);
-		}
-		
+		Aluno aluno4 = new Aluno("José", 50);	 
 	}	
 					
 		
