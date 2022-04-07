f5e44262c7a0b8de20db7ccda6925c29c8b1214f
Paulo Franca <admpaulofranca@gmail.com>
2022-04-06 23:26:38 -0300

    Criando um programa de calculo de média imputando na tela

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,22 +2,43 @@ package cursojava.executavel;
 
 import javax.swing.JOptionPane;
 
+import org.graalvm.compiler.lir.StandardOp.JumpOp;
+
 public class PrimeiraClasseJava {
 
 	/* Main é um método auto executável de Java */
 	public static void main(String[] args) {
 
 		
-		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros");
-		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas");		
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
 		
-		double  carroNumero = Double.parseDouble(carros);
-		double  pessoaNumero = Double.parseDouble(pessoas);
 		
-		int divisao = (int) (carroNumero / pessoaNumero);
 		
-		double resto = carroNumero % pessoaNumero;
+		double nNota1 = Double.parseDouble(nota1);
+		double nNota2 = Double.parseDouble(nota2);
+		double nNota3 = Double.parseDouble(nota3);
+		double nNota4 = Double.parseDouble(nota4);
 		
-		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + "carros e sobrou" + resto + "carros");		
-	}
+		double media = (nNota1 + nNota2 + nNota3 + nNota4) / 4;
+		
+		/*Média para aprovação é 70*/
+		
+		if (media >= 50) {
+			
+			if (media >= 70) {
+				JOptionPane.showConfirmDialog(null, "O aluno está aprovado com média de :" + media);
+			}else {
+				JOptionPane.showConfirmDialog(null, "O aluno está em recueração com média de :" + media);
+			}
+		}else {
+			JOptionPane.showConfirmDialog(null, "Aluno está reprovado com média de : " + media);
+		}
+		
+	}	
+					
+		
 }
+
