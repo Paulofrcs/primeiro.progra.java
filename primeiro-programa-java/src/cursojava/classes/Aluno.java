a9d62cac4d6bbf030b2af8de0d89014ff1c795ef
Paulo Franca <admpaulofranca@gmail.com>
2022-04-07 01:24:07 -0300

    classe/objeto que representa o Aluno

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,30 @@
+package cursojava.classes;
+
+/*Esta é nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String regstroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+	public Aluno() {/*Cria os dados na memória - Sendo padrão do Java*/
+		
+	}
+	
+	public Aluno(String nomePadrao); {
+		nome = nomePadrao;
+	}
+	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+	
+}
