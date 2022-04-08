eddddc73a9ccaa64aa79cd85f825bb9344d341f0
Paulo Franca <admpaulofranca@gmail.com>
2022-04-07 23:20:19 -0300

    metódo de retorno de média do aluno

--- primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -3,22 +3,29 @@ package cursojava.classes;
 /*Esta é nossa classe/objeto que representa o Aluno*/
 public class Aluno {
 	
-	String nome;
-	int idade;
-	String dataNascimento;
-	String regstroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	/*Esses são os atributos do Aluno*/
+	private String nome;/*Por padrão o Java sempre coloca os atributos como 'private'. É necessário  declarar como puclic. p. ex. public String nome*/
+	private int idade;
+	private String dataNascimento;
+	private String regstroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
 
+	
+	
 	public Aluno() {/*Cria os dados na memória - Sendo padrão do Java*/
 		
 	}
 	
-	public Aluno(String nomePadrao); {
+	public Aluno(String nomePadrao) {
 		nome = nomePadrao;
 	}
 	
@@ -27,4 +34,127 @@ public class Aluno {
 		idade = idadePadrao;
 	}
 	
-}
+	/*Veremos os métodos SETTERS e GETTERS do Objeto*/
+	/*SET  é para adicionar ou receber dado para os atributos*
+	 /*GET é para resgatar ou obter o valor do atributo*/
+	 
+	 /*Recebe dados*/
+	 public void setNome(String nome) {
+		 this.nome = nome;
+		 
+	 }
+	 
+	 public String getNome() {
+		 return nome; /*ou this.nome; no get não há a necessidade...*/
+	 }
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegstroGeral() {
+		return regstroGeral;
+	}
+
+	public void setRegstroGeral(String regstroGeral) {
+		this.regstroGeral = regstroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	/*Método que retorna a  média do aluno*/
+	public double getMediaNota () {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+		
+	}
+	}
