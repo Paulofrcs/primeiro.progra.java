1fad389f2500dbc0106af488c072cbb771ff2915
Paulo Franca <admpaulofranca@gmail.com>
2022-04-13 21:45:48 -0300

    Revisão Set e Get

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -12,8 +12,35 @@ public class PrimeiraClasseJava {
 		/*O "Ojbjeto" em Java é isso aí em baixo; no caso é a estrutura Aluno aaluno1 = new Aluno();*/
 		Aluno aluno1 = new Aluno(); /*Aqui será o João*/
 		aluno1.setNome("João da Silva"); /*com o método set eu chamo com "set.nome..."*/		
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("444.56987.884");
+		aluno1.setNumeroCpf("123.456.789-98");
+		aluno1.setNomeMae("Shirlei");
+		aluno1.setNomePai("Antonio");
+		aluno1.setDataMatricula("10/01/2019");
+		aluno1.setSeriaMatriculado("5");
+		aluno1.setNomeEscola("Escola JDEV Treinamentos");
+		
+		
 		System.out.println("O nome do aluno é " + aluno1.getNome());		/*Aqui usei o getNome...*/
+		System.out.println("Idade é "  + aluno1.getIdade());
+		System.out.println("A data de nascimento é "  + aluno1.getDataNascimento());
+		
+		System.out.println("===================================================");
+		/*--------------------------------------------------------------------------------------------------------------*/
+		
 		Aluno aluno2 = new Aluno();/*Aqui será o Pedro...*/
+		
+		aluno2.setNome("Pedro");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("10/10/1980");
+		
+		System.out.println("Aluno2: O nome do aluno é " + aluno2.getNome());		/*Aqui usei o getNome...*/
+		System.out.println("Aluno2: Idade é "  + aluno2.getIdade());
+		System.out.println("Aluno2: A data de nascimento é "  + aluno2.getDataNascimento());
+		
+		/*------------------------------------------------------------------------------------------------------*/
 		
 		
 		Aluno aluno3 = new Aluno(); /*Aqui será o Alex...*/
